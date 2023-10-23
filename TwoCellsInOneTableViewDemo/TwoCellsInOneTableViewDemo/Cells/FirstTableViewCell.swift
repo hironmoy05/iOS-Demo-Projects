eaecb79b4f6468a0438569c77e202f0336ab6033
Hironmoy Dhar <hironmoy.dhar@repcard.com>
2023-10-23 07:38:09 +0530

    Done Switch two cell in tableView

---
+++ TwoCellsInOneTableViewDemo/TwoCellsInOneTableViewDemo/Cells/FirstTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FirstTableViewCell.swift
+//  TwoCellsInOneTableViewDemo
+//
+//  Created by Repcard on 23/10/23.
+//
+
+import UIKit
+
+class FirstTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ TwoCellsInOneTableViewDemo/TwoCellsInOneTableViewDemo/Cells/SecondTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SecondTableViewCell.swift
+//  TwoCellsInOneTableViewDemo
+//
+//  Created by Repcard on 23/10/23.
+//
+
+import UIKit
+
+class SecondTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
